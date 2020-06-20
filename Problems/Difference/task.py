accfb4e152b49f7f81d036d6eb76d380324e42f1
Josh <niipinto001@gmail.com>
2020-06-19 21:36:00 -0400

    Initial commit

---
+++ Problems/Difference/task.py
@@ -0,0 +1,3 @@
+usr1 = int(input())
+usr2 = int(input())
+print(usr1 - usr2)
---
+++ Problems/Good rest on vacation/task.py
@@ -0,0 +1,6 @@
+days = int(input())
+food_cost = int(input())
+flight_cost = int(input())
+hotel = int(input())
+
+print()
